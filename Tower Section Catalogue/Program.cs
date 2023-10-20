22c0b1dfe20ec8669dbf56062af88d92fd4e227b
AndreiaQ <anbe9@hotmail.com>
2023-10-20 19:40:48 +0100

    Executes automatically on Index

--- Tower Section Catalogue/Program.cs
+++ Tower Section Catalogue/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Shells}/{action=Index}/{id?}");
 
 app.Run();
